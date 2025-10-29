13dec1ab3f89d397a31d04952fd25a9fe6a95c59
Strilets Glib FI41mn <181478164+Strilets-Glib-FI41mn@users.noreply.github.com>
2025-10-29 20:19:51 +0200

    extra compersions

--- src/main.rs
+++ src/main.rs
@@ -13,4 +13,19 @@ fn main() {
     let input_file_name = "working/wikipedia.txt";
     let output_file_name = "working/wikipedia";
     println!("{:?}", encode(input_file_name, output_file_name));
+
+
+    let input_file_name = "working/task01";
+    let output_file_name = "working/task01_compact";
+    println!("{:?}", encode(input_file_name, output_file_name));
+    
+    /*
+    let input_file_name = "tested/Ефимов_А_В_,_Демидович_Б_П_Сборник.pdf";
+    let output_file_name = "tested/Ефимов_А_В_,_Демидович_Б_П_Сборник";
+    println!("{:?}", encode(input_file_name, output_file_name));
+
+    let input_file_name = "tested/image.png";
+    let output_file_name = "tested/image";
+    println!("{:?}", encode(input_file_name, output_file_name));
+    */
 }
