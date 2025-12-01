80f95d2ba77be9e644c6d0690fd66d8663d92b6d
Aultus Defora <181478164+Strilets-Glib-FI41mn@users.noreply.github.com>
2025-12-01 16:56:47 +0200

    Update lib.rs
    create a test for encoding and decoding with padding

--- huffman_encoding/src/lib.rs
+++ huffman_encoding/src/lib.rs
@@ -2,3 +2,31 @@ pub mod power_calc;
 pub mod binary_tree;
 pub mod encoder;
 pub mod decoder;
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn huffman_text_big() -> std::io::Result<()>{
+        let text = "The Project Gutenberg eBook of The Ethics of Aristotle
+    
+This ebook is for the use of anyone anywhere in the United States and
+most other parts of the world at no cost and with almost no restrictions
+whatsoever. You may copy it, give it away or re-use it under the terms
+of the Project Gutenberg License included with this ebook or online
+at www.gutenberg.org. If you are not located in the United States,
+you will have to check the laws of the country where you are located
+before using this eBook.".to_string();
+        let thing = text.as_bytes();
+        let cursor = std::io::Cursor::new(&thing);
+        let mut he_text = vec![];
+        let mut cursor_writter = std::io::Cursor::new(&mut he_text);
+        huffman_encoding::encoder::encode_with_padding(cursor, &mut cursor_writter)?;
+        let cursor = std::io::Cursor::new(&he_text);
+        let mut he_dec = vec![];
+        huffman_encoding::decoder::decode_with_padding(cursor, &mut he_dec)?;
+        
+        println!("{:?}", str::from_utf8(&he_dec));
+        // assert_eq!(str::from_utf8(&he_dec), Ok(&text).map(|x| x.as_str()));
+        assert_eq!(he_dec, thing);
+        Ok(())
+    }
+}
